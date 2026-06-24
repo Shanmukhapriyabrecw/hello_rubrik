af998c657d1e82c095adcc001a9d96566843bba0
Shanmukhapriyabrecw <priya.061108@gmail.com>
2026-06-24 10:14:02 +0530

    Added Go version: Step 2 towards Rubrik SDE 💚

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+     fmt.println("Hello Rubrik 👋")
+     fmt.println("I am Shanmukha priya from BRECW CSM")
+     fmt.println("My 4-years SDE journey starts today: June 24, 2026")
+     fmt.println("Target: Rubrik Bangalore July 2030 🚀")
